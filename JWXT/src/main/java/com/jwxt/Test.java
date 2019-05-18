90fd13f7d02919564920f9f6b994c52300c8b3b8
Wang9605 <1328184844@qq.com>
2019-05-18 15:28:10 +0800

    log

--- JWXT/src/main/java/com/jwxt/Test.java
+++ JWXT/src/main/java/com/jwxt/Test.java
@@ -1,5 +1,7 @@
 package com.jwxt;
 
 public class Test {
-
+	public void sys(){
+		
+	}
 }
--- JWXT/src/main/java/com/jwxt/common/exception/exc.java
+++ JWXT/src/main/java/com/jwxt/common/exception/exc.java
@@ -1,5 +1,7 @@
 package com.jwxt.common.exception;
 
 public class exc {
-
+	public static void main(String[] args) {
+		System.out.println();
+	}
 }
