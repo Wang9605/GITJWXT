61764bdaf3f716a9c5bcc1959c3681f339b5aa6f
chenbabelon <Administrator@A36Y7DGX605K5WC>
2019-05-19 09:59:03 +0800

    cbl

--- JWXT/src/main/java/com/jwxt/entity/Menu.java
+++ JWXT/src/main/java/com/jwxt/entity/Menu.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Menu implements Serializable{
-	
+	//陈柏霖
 	private Integer menuId;
 	private String menuName;
 	private String menuUrl;
